0e2ef80cf3fc39e44d9e2db870bea56a9e080d3d
Chen <chenq_developer@126.com>
2024-02-05 14:18:21 +0800

    组件局部样式

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,14 @@ export default {
   }
 }
 </script>
+
+<!--
+    https://v2.vuejs.org/v2/guide/comparison.html#HTML-amp-CSS
+    
+    App.vue中的样式通常应用于所有组件，所以不要使用scoped修饰
+-->
+<style>
+p {
+  color: red;
+}
+</style>
--- src/components/StudentInfo.vue
+++ src/components/StudentInfo.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <p>学生姓名：{{ age }}</p>
+    <div class="stu">
+        <p class="name">学生姓名：{{ name }}</p>
         <p>学生年龄：{{ age }}</p>
     </div>
 </template>
@@ -16,3 +16,22 @@ export default {
     }
 }
 </script>
+
+<!--
+    https://v2.vuejs.org/v2/guide/comparison.html#HTML-amp-CSS
+    
+    scoped
+    使样式仅应用在当前组件中，这样不同组件的相同名称之间不会互相影响
+
+    lang
+    可以指定使用的语法，比如这里使用less，需要安装less
+    npm install less-loader -D
+-->
+<style scoped lang="less">
+.stu {
+    background-color: orange;
+    .name {
+        font-size: 40px;
+    }
+}
+</style>
