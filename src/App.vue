0c56363da7858c3381ef97d86276c7809c803ddc
Chen <chenq_developer@126.com>
2024-02-05 10:23:10 +0800

    通过props属性像组件传递参数

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
     <FamousSchool></FamousSchool>
+
+    <!-- 数字需要使用v-bind通过表达式传递 -->
+    <StudentInfo name="张三" :age="17" gender="女"></StudentInfo>
+
+    <!-- 年龄是非必传会使用默认值 -->
+    <StudentInfo name="李四" gender="男"></StudentInfo>
+
+    <!--
+      传递一个不存在的props会被添加到组件根元素的$attrs数组中
+      https://v2.vuejs.org/v2/guide/components-props.html#Non-Prop-Attributes
+    -->
+    <StudentInfo name="李四" gender="男" edu="大学"></StudentInfo>
   </div>
 </template>
 
 <script>
-import FamousSchool from './components/FamousSchool.vue'; 
-
+import FamousSchool from './components/FamousSchool.vue'
+import StudentInfo from './components/StudentInfo.vue'
 export default {
   name: 'App',
   components: {
-    FamousSchool
+    FamousSchool,
+    StudentInfo
   }
 }
 </script>
---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <h2 class="student">{{ msg }}</h2>
+        <p>姓名：{{ name }}</p>
+        <p>年龄：{{ studentAge }}</p>
+        <p>性别：{{ gender }}</p>
+        <button @click="ageAdd">年龄+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StudentInfo',
+    inheritAttrs: false,
+    data() {
+        return {
+            msg: '这是一个学生信息',
+            // props会先于data被初始化，所以这里可以这样引用
+            studentAge: this.age
+        }
+    },
+    /**
+     * 通过数组简单传递
+     * props: ['name', 'age', 'gender']
+     * 
+     * 通过对象添加类型校验
+     * props: {
+     *     name: String,
+     *     age: Number,
+     *     gender: String
+     * }
+     * 
+     * 下面的是完整的校验方式
+     * https://v2.vuejs.org/v2/guide/components-props.html#Prop-Validation
+     */
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: Number,
+            default: 19
+        },
+        gender: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        /**
+         * https://v2.vuejs.org/v2/guide/components-props.html#One-Way-Data-Flow
+         * Vue不推荐直接修改外部传入的props属性
+         */
+        ageAdd() {
+            this.studentAge++
+        }
+    }
+}
+</script>
+
+<style>
+.student {
+    background-color: gray;
+}
+</style>
