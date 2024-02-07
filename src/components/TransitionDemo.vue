a2f52d3496bc8b3926c738eaade8d2a696975b84
chen <chenq_developer@126.com>
2024-02-07 15:16:06 +0800

    vue过渡

---
+++ src/components/TransitionDemo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <Transition appear name="hello">
+            <h1 v-show="isShow">你好啊</h1>
+        </Transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnimationDemo',
+    data() {
+        return {
+            isShow: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    background-color: orange;
+}
+
+.hello-enter,
+.hello-leave-to {
+    transform: translateX(-100%);
+}
+
+.hello-enter-active,
+.hello-leave-active {
+    transition: 1s linear;
+}
+
+.hello-leave,
+.hello-enter-to {
+    transform: translateX(0%);
+}
+</style>
