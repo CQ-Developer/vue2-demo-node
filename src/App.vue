cca1d83b09ac21967cacb7ba20d8905054fc1b1e
chen <chenq_developer@126.com>
2024-02-07 16:05:41 +0800

    使用第三方动画库animate.css完成动画

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <AnimationDemo></AnimationDemo>
     <TransitionDemo></TransitionDemo>
+    <AnimateCssDemo></AnimateCssDemo>
   </div>
 </template>
 
 <script>
 import AnimationDemo from './components/AnimationDemo.vue'
 import TransitionDemo from './components/TransitionDemo.vue'
+import AnimateCssDemo from './components/AnimateCssDemo.vue'
 export default {
   name: 'App',
   components: {
     AnimationDemo,
-    TransitionDemo
+    TransitionDemo,
+    AnimateCssDemo
   }
 }
 </script>
---
+++ src/components/AnimateCssDemo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <!--
+            使用第三方动画库 animate.css
+            npm install animate.css --save
+        -->
+        <transition
+            appear
+            name="animate__animated animate__bounce"
+            enter-active-class="animate__rotateInDownLeft"
+            leave-active-class="animate__rotateOutDownLeft">
+            <h1 v-show="isShow">你好啊</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+    name: 'AnimationDemo',
+    data() {
+        return {
+            isShow: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    background-color: orange;
+}
+</style>
