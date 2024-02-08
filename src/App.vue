3f5d2ef46f6fdd874f53c64673c4bc6992791353
chen <chenq_developer@126.com>
2024-02-08 11:02:58 +0800

    案例初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <CounterDemo></CounterDemo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CounterDemo from './components/CounterDemo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterDemo
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CounterDemo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>当前求和为：{{ sum }}</h1>
+        <select v-model.number="n">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <button @click="plusN">+</button>
+        <button @click="minusN">-</button>
+        <button @click="plusNWhenOdd">当前求和为奇数+</button>
+        <button @click="plusNDelayed">延迟+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CounterDemo',
+    data() {
+        return {
+            sum: 0,
+            n: 1
+        }
+    },
+    methods: {
+        plusN() {
+            this.sum += this.n
+        },
+        minusN() {
+            this.sum -= this.n
+        },
+        plusNWhenOdd() {
+            if (this.sum % 2) {
+                this.sum += this.n
+            }
+        },
+        plusNDelayed() {
+            setTimeout(() => this.sum += this.n, 1000)
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    margin-left: 5px;
+}
+</style>
