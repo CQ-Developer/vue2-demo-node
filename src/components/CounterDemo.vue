e79c6619c3322bbf48c9a70f49fb3ca93abc7108
chen <chenq_developer@126.com>
2024-02-08 14:16:44 +0800

    辅助函数的对象写法

--- src/components/CounterDemo.vue
+++ src/components/CounterDemo.vue
@@ -40,7 +40,7 @@ export default {
     },
     computed: {
         ...mapState(['sum', 'school', 'subject']),
-        ...mapGetters(['timesTen'])
+        ...mapGetters({ timesTen: 'timesTen' })
     }
 }
 </script>
