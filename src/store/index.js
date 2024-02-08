a548cd641949603be9507ca81613db6bfba9d20b
chen <chenq_developer@126.com>
2024-02-08 14:50:23 +0800

    统一编码风格

--- src/store/index.js
+++ src/store/index.js
@@ -4,29 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    actions: {
-        plusWhenOdd({ commit, state }, payload) {
-            if (state.sum % 2) {
-                commit('plusValue', payload)
-            }
-        },
-        // 可以使用箭头函数
-        plusDelayed: ({ commit }, payload) => setTimeout(() => commit('plusValue', payload), 1000)
-    },
     state: {
         sum: 0,
         school: '清华',
         subject: '计算机'
     },
-    mutations: {
-        plusValue(state, value) {
-            state.sum += value
-        },
-        minusValue(state, value) {
-            state.sum -= value
-        }
-    },
     getters: {
         timesTen: state => state.sum * 10
+    },
+    actions: {
+        plusWhenOdd: ({ commit, state: { sum } }, payload) => {
+            if (sum % 2) commit('plusValue', payload)
+        },
+        plusDelayed: ({ commit }, payload) => setTimeout(() => commit('plusValue', payload), 1000)
+    },
+    mutations: {
+        plusValue: (state, value) => state.sum += value,
+        minusValue: (state, value) => state.sum -= value
     }
 })
