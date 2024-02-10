8ce24374bf261b09259ca0f88d826400c8f3745e
chen <18551681083@163.com>
2024-02-10 15:50:18 +0800

    通过对象方式配置路径query查询参数向路由组件传递数据

--- src/pages/HomeMessage.vue
+++ src/pages/HomeMessage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- 通过query参数传递数据 -->
-        <router-link v-for="m in message" :key="m.id" :to="`/home/message/detail?id=${m.id}&title=${m.title}`">
+        <router-link v-for="m in message" :key="m.id" :to="{ path: '/home/message/detail', query: { id: m.id, title: m.title } }">
             {{ m.title }}
         </router-link>
         <router-view></router-view>
