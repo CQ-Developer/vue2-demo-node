b7064868d1c6d76f2305027402bc577b01eaea55
chen <18551681083@163.com>
2024-02-13 17:46:24 +0800

    组件内守卫

--- src/pages/AboutDemo.vue
+++ src/pages/AboutDemo.vue
@@ -6,6 +6,22 @@
 
 <script>
 export default {
-    name: 'AboutDemo'
+    name: 'AboutDemo',
+    /**
+     * 组件内守卫
+     * 通过路由规则进入组件时执行
+     */
+    beforeRouteEnter(to, from, next) {
+        console.log('beforeRouteEnter开始执行...', to, from)
+        next()
+    },
+    /**
+     * 组件内守卫
+     * 通过路由规则离开组件时执行
+     */
+    beforeRouteLeave(to, from, next) {
+        console.log('beforeRouteLeave开始执行...', to, from)
+        next()
+    }
 }
 </script>
